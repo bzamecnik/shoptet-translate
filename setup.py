f9a3e016774e1370e8a1063133fabe24f3844a6b
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2018-09-23 17:59:45 +0200

    Increment version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='shoptet_translate',
-      version='0.0.2',
+      version='0.0.3',
       description='Translate Shoptet invoices',
       url='https://github.com/bzamecnik/shoptet-invoice-en',
       author='Bohumir Zamecnik',
